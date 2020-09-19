cae9b80b7c873ea27120afba3a329fd8ca505e60
badar343 <66517064+badar343@users.noreply.github.com>
2020-09-19 16:02:54 +0500

    added a few lines in chemb.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Badar from 'chemb.js';
 function App() {
   return (
     <div className="App">
+      <Badar name_one = "Badar Tariq" name_two ="Akash Ch"></Badar>
     </div>
   );
 }
--- src/chemb.js
+++ src/chemb.js
@@ -1,1 +1,11 @@
 import React from 'react';
+function Badar(props){
+    return(
+        <div>
+            <h1>This is {props.name_one}</h1>
+            <hr/>
+    <h1>This is {props.name_two}</h1>
+        </div>
+    )
+}
+export default chemb
